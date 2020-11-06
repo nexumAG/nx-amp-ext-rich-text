5322c6684677b9d5016d4ef430b265802e857896
Javier Pascual <javier.pascual@oramon.nl>
2020-11-06 11:30:47 +0100

    feat(MF-255): Add test to createSchema

--- packages/language-markdown/src/schema/createSchema.ts
+++ packages/language-markdown/src/schema/createSchema.ts
@@ -4,6 +4,7 @@ import { inline_styles } from "../inline_styles";
 import { createMarkdownParser } from "../markdown/MarkdownParser";
 import { createMarkdownSerializer } from "../markdown/MarkdownSerializer";
 import { soft_hyphen } from "../soft_hyphen";
+import { test } from "../test";
 
 // tslint:disable-next-line
 const { tableNodes } = require("prosemirror-tables");
@@ -17,29 +18,40 @@ export function createSchema(isInlineStylesEnabled: boolean = false): any {
   // TODO: don't register nodes and marks that are disabled in the options
   let marks = schema.spec.marks;
   if (isInlineStylesEnabled) {
-    marks = marks.addToEnd("inline_styles", inline_styles)
+    marks = marks.addToEnd("inline_styles", inline_styles);
   }
 
-  const nodes = schema.spec.nodes.append(tableNodes({
-    tableGroup: "block",
-    cellContent: "block",
-    cellAttributes: {
-      background: {
-        default: null,
-        // tslint:disable-next-line
-        getFromDOM(dom: any) { return dom.style.backgroundColor || null },
-        // tslint:disable-next-line
-        setDOMAttr(value: any, attrs: any) { if (value) attrs.style = (attrs.style || "") + `background-color: ${value};` }
-      }
-    }
-  })).addToEnd("soft_hyphen", soft_hyphen)
-     .addToEnd("anchor", anchor)
-     .update("paragraph", paragraph_align)
-     .update("heading", heading_align);
+  const nodes = schema.spec.nodes
+    .append(
+      tableNodes({
+        tableGroup: "block",
+        cellContent: "block",
+        cellAttributes: {
+          background: {
+            default: null,
+            // tslint:disable-next-line
+            getFromDOM(dom: any) {
+              return dom.style.backgroundColor || null;
+            },
+            // tslint:disable-next-line
+            setDOMAttr(value: any, attrs: any) {
+              if (value)
+                attrs.style =
+                  (attrs.style || "") + `background-color: ${value};`;
+            },
+          },
+        },
+      })
+    )
+    .addToEnd("soft_hyphen", soft_hyphen)
+    .addToEnd("anchor", anchor)
+    .update("paragraph", paragraph_align)
+    .update("test", test)
+    .update("heading", heading_align);
 
   return new Schema({
     nodes,
-    marks
+    marks,
   });
 }
 
